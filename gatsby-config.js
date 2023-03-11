5444a1aba7db87794f1af2201ee9e295c1a83f6b
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-03-10 20:57:10 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `first-project`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
