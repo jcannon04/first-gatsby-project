70ea53f8a0577e8988fe9890ab48d4491e4542f5
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-03-11 14:56:31 -0600

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `first-project`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`
+  ],
+};
